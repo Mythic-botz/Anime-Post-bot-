e74ce184b650fbdbad4013a88924c71bb84fa9d6
Mythic-botz <priyanshuprasad853@gmail.com>
2025-07-19 16:03:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
-API_ID = int(os.getenv("API_ID", ""))
-API_HASH = os.getenv("API_HASH", "")
+API_ID = int(os.getenv("API_ID", "23476863"))
+API_HASH = os.getenv("API_HASH", "69daa0835439c4211f34c2e9ad0acb5c")
 BOT_TOKEN = os.getenv("BOT_TOKEN", "")
 CHANNEL_ID = int(os.getenv("CHANNEL_ID", ""))
 WATERMARK = os.getenv("WATERMARK", "@Mythic_Animes")
